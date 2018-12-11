3aa7010d8bb8c757055dfe1f22d82919995c86d1
ruiyuanlu <luruiyuan@outlook.com>
2018-12-11 18:47:25 +0800

    为decode_sequences方法增加了merge_repeated功能
    为decode_sequences方法增加了merge_repeated功能，连续出现的相同token可以直接合并。

--- keras/Word_Level_Machine_Neural_Translation_Using_Keras/postprocess.py
+++ keras/Word_Level_Machine_Neural_Translation_Using_Keras/postprocess.py
@@ -10,10 +10,22 @@ def save_model_topology_weights(model, topology_path, weights_path, encoding='ut
     model.save(weights_path)
 
 
-def decode_sequences(encoder, decoder, trgt_word2id, trgt_id2wrod,
-                     *input_sequences, zero_mask=True):
+def decode_sequences(encoder, decoder,
+                     trgt_word2id,
+                     trgt_id2wrod,
+                     *input_sequences,
+                     merge_repeated=True,
+                     zero_mask=True):
     '''
     基于贪心算法, 根据输入序列解码给出输出序列
+    Args:
+        encoder: Model 基于Keras的编码器模型
+        decoder: Model 基于Keras的解码器模型
+        trgt_word2id: 目标语言的词语到索引的字典
+        trgt_id2word: 目标语言索引到词语的字典
+        input_sequences: Iterable 每次返回一个源语言词语索引序列的可迭代对象
+        merge_repeated: bool 是否合并连续出现的重复词语, 默认为True
+        zero_mask: bool 是否使用了0进行padding, 默认为True
     '''
     from params import BOS, EOS, trgt_maxlen
     trgt_vocab_size = len(trgt_id2wrod) + 1 if zero_mask else len(trgt_id2wrod)
@@ -26,19 +38,21 @@ def decode_sequences(encoder, decoder, trgt_word2id, trgt_id2wrod,
         if isinstance(states, np.ndarray):
             states = [states]
         # 为Teacher-Forcing输入起始的Teacher字符
-        trgt_seq_teacher = np.zeros((1, 1, trgt_vocab_size), dtype='float32')
+        trgt_seq_teacher = np.zeros((1, 1, trgt_vocab_size), dtype=bool)
         trgt_seq_teacher[0, 0, trgt_word2id[BOS]] = 1
         # 通过贪心算法逐次生成序列
-        decoded_seq = []
-        for _ in range(400):
+        decoded_seq, last_idx = [], None
+        for _ in range(1000):
             outputs = decoder.predict([trgt_seq_teacher] + states)
             # 获得输出的序列, 同时更新循环网络编码状态, 用于下一词语
             output, states = outputs[0], outputs[1:]
             # 根据输出进行采样, 并加入到解码结果decoded_seq中
             sampled_idx = np.argmax(output[0, -1, :])
-            if zero_mask and sampled_idx == 0:
-                # 跳过padding字符
+            if (zero_mask and sampled_idx == 0 or
+               merge_repeated and last_idx == sampled_idx):
+               # 跳过padding字符, 合并连续出现的重复字符
                 continue
+            last_idx = sampled_idx
             sampled_word = trgt_id2wrod[sampled_idx]
             decoded_seq.append(sampled_word)
             # 采样终止条件
@@ -49,7 +63,3 @@ def decode_sequences(encoder, decoder, trgt_word2id, trgt_id2wrod,
             trgt_seq_teacher[0, 0, sampled_idx] = 1
         # 一次序列采样完成后, 返回已生成的序列
         yield decoded_seq
-
-
-def reweight():
-    pass
